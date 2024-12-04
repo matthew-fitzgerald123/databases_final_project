48bd441cbd4dccb56732fc62c184948ba7975466
SpeedySpaceBoy <134542673+SpeedySpaceBoy@users.noreply.github.com>
2024-12-04 16:34:48 -0500

    Added comments to phase 5.1 and 5.2. as well as made 5.2 functional

--- Phase 5 Files/phase5_1.sql
+++ Phase 5 Files/phase5_1.sql
@@ -1,4 +1,8 @@
-CREATE VIEW `employee performance` AS
+
+-- Create a view for employee performance
+-- Select all affected items for calculation
+-- Do calculation (Note: calculation does not include freight)
+CREATE VIEW `employee performance` AS 
 SELECT employeeID, employees_firstName, employees_lastName, orderID, quantity,
 ROUND((quantity * (unitPrice - (unitPrice * discount))), 2) AS revenue
 FROM project_data
